0cf29ac98a66a8ae5e9c73a0afaa8aa49edf8715
Rengkat <successinmotion11@gmail.com>
2026-05-01 12:26:42 +0100

    register user auth service

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { RegisterUserDto } from 'src/user/dto/register-user.dto';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly userService: UserService) {}
+  // ======== BASIC AUTH METHODS ========
+  //Register
+  async register(dto: RegisterUserDto) {
+    // Check if user already exists
+    const existingUser = await this.userService.findByEmailForAuth(dto.email);
+    if (existingUser) {
+      throw new Error('User already exists');
+    }
+    // Create new user
+    const user = await this.userService.registerUser(dto);
+    return user;
+  }
+  //Login
+  //verify email using otp
+  //rend email verification otp
+  //reset password using otp
+  //forgot password using otp
+  //phone number verification using otp
+  //restore password using otp
+  //2FA using otp
+  //refresh access token using refresh token
+  //handle social logins (Google, Facebook, etc.)
+  //   ====== HELPER METHODS ======
+  //generate JWT access and refresh tokens
+  //generate and verify OTPs
+  //otp expiration and retry logic
+  //send emails for verification and password reset
+}
