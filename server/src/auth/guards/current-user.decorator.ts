e7d2a1b0405321fca0898fc185a9241c5507d89e
Rengkat <successinmotion11@gmail.com>
2026-05-08 01:03:43 +0100

    PERMISSION DECORATOR

--- server/src/auth/guards/current-user.decorator.ts
+++ server/src/auth/guards/current-user.decorator.ts
@@ -1,14 +1,14 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { User } from 'src/user/entities/user.entity';
 
-export const currentUser = createParamDecorator(
+export const CurrentUser = createParamDecorator(
   (field: unknown, cxt: ExecutionContext): User => {
     const request = cxt.switchToHttp().getRequest();
     return request.user;
   },
 );
 
-export const userId = createParamDecorator(
+export const UserId = createParamDecorator(
   (_field: unknown, cxt: ExecutionContext): string => {
     const request = cxt.switchToHttp().getRequest();
     return request.user?.id;
