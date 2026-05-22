f84bf6f60d1f1bd7a1aa912ce2d57229bb86e759
Rengkat <successinmotion11@gmail.com>
2026-05-22 19:27:52 +0100

    update: question module with entities

--- server/src/questions/questions.module.ts
+++ server/src/questions/questions.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { QuestionsService } from './questions.service';
 import { QuestionsController } from './questions.controller';
+import { QuestionExplanation } from './entities/question-explanation.entity';
+import { QuestionAnswer } from './entities/question-answer.entity';
+import { QuestionOption } from './entities/question-option.entity';
+import { Question } from './entities/question.entity';
+import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
 
 @Module({
   controllers: [QuestionsController],
   providers: [QuestionsService],
+  imports: [
+    TypeOrmModule.forFeature([
+      Question,
+      QuestionOption,
+      QuestionAnswer,
+      QuestionExplanation,
+    ]),
+  ],
 })
 export class QuestionsModule {}
