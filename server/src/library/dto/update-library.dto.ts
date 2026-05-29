d6e22d6799ef42dbd7a432c1fe4ed748f5753ae4
Rengkat <successinmotion11@gmail.com>
2026-05-29 21:14:12 +0100

    add: update library contnent dto

--- server/src/library/dto/update-library.dto.ts
+++ server/src/library/dto/update-library.dto.ts
@@ -1,4 +1,18 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateLibraryDto } from './create-library.dto';
+import { IsBoolean, IsOptional } from 'class-validator';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 
-export class UpdateLibraryDto extends PartialType(CreateLibraryDto) {}
+export class UpdateLibraryDto extends PartialType(CreateLibraryDto) {
+  @ApiPropertyOptional({
+    description: 'Soft-toggle visibility without deleting',
+  })
+  @IsOptional()
+  @IsBoolean()
+  isActive?: boolean;
+
+  @ApiPropertyOptional({ description: 'Publish / unpublish content' })
+  @IsOptional()
+  @IsBoolean()
+  isPublished?: boolean;
+}
